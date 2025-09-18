aa43a01319abead570b6bbfb859da9aa0d740e13
LEQUANGHAO\Acer <haolq2002@gmail.com>
2025-09-18 22:43:46 +0700

    update code

--- src/main/java/com/example/identity_service/mapper/UserMapper.java
+++ src/main/java/com/example/identity_service/mapper/UserMapper.java
@@ -1,11 +1,9 @@
 package com.example.identity_service.mapper;
 
 import com.example.identity_service.dto.request.UserCreationRequest;
-import com.example.identity_service.dto.request.UserUpdateRequest;
 import com.example.identity_service.dto.response.UserResponse;
 import com.example.identity_service.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
